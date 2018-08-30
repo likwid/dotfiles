faaed336ee47de282a74102a9940bb709f75cc8e
Jeff Gonzalez <likwid@gmail.com>
2018-08-29 23:01:34 -0500

    Add rjg mac default settings

---
+++ scripts/rjg-mac-defaults.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+# Adapted from https://github.com/mathiasbynens/dotfiles/blob/master/.macos
+
+if [ -z "$INSTALL_MAC_DEFAULTS" ]; then
+    exit 2;
+fi
+
+set -x
+
+if [[ -z "${CI}" ]]; then
+  sudo -v # Ask for the administrator password upfront
+  # Keep-alive: update existing `sudo` time stamp until script has finished
+  while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+fi
+
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
+
+# Close any open System Preferences panes, to prevent them from overriding settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+# Expand print panel by default
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+# Save to disk (not to iCloud) by default
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+# Automatically quit printer app once the print jobs complete
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+# Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+# Reveal IP address, hostname, OS version, etc. when clicking the clock in the login window
+sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
+
+# Set Desktop as the default location for new Finder windows
+defaults write com.apple.finder NewWindowTarget -string "PfDe"
+defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/Desktop/"
+
+defaults write com.apple.finder AppleShowAllFiles -bool true        # Finder: Show hidden files by default
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true     # Finder: Show all filename extensions
+defaults write com.apple.finder ShowStatusBar -bool true            # Finder: Show status bar
+defaults write com.apple.finder ShowPathbar -bool true              # Finder: Show path bar
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true  # Finder: Display full POSIX path as window title
+defaults write com.apple.finder _FXSortFoldersFirst -bool true      # Finder: Keep folders on top when sorting by name
+chflags nohidden ~/Library     # Show the ~/Library folder
+sudo chflags nohidden /Volumes # Show the /Volumes folder
+
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Automatically open a new Finder window when a volume is mounted
+defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+
+# Use list view in all Finder windows by default (codes for the other view modes: `icnv`, `clmv`, `Flwv`)
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+
+#"Disable 'natural' (Lion-style) scrolling"
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+defaults write com.apple.finder AppleShowAllFiles -bool YES 
+defaults write com.apple.menuextra.battery ShowPercent YES
+
+killall SystemUIServer
+killall Finder
